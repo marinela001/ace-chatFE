89d018ccc58e30f60d33bfdd5cd28f7297f97d9e
unknown <nelaaga@icloud.com>
2023-10-31 17:28:23 +0100

    added not-found

---
+++ pages/[...not_found]/page.tsx
@@ -0,0 +1,12 @@
+
+'use client'
+import { useRouter } from 'next/navigation';
+import React from 'react'
+
+const Page = () => {
+  const router = useRouter();
+  router.push("/")
+  return null;
+}
+
+export default Page
